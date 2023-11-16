0a5a2b356e9c80a498f710ceb4e94b455f990061
Marco <marco.odermatt56@gmail.com>
2023-11-16 10:46:41 +0000

    fixed user creation into database

--- src/main/java/ch/zli/m223/m223project/Controller/UserController.java
+++ src/main/java/ch/zli/m223/m223project/Controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,21 +24,12 @@ public class UserController {
     ApplicationUser user = new ApplicationUser();
 
     @GetMapping("/new")
-    public String showNewUserForm(Model model) {
-        model.addAttribute("user", user);
+    public String showNewUserForm(@ModelAttribute("user") ApplicationUser user) {
         return "newUsers";
     }
 
     @PostMapping("/new")
-    public String saveUser(Model model) {
-        
-        String username = "";
-        String password = "";
-        String email = ""; 
-
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setEmail(email);
+    public String saveUser(@ModelAttribute("user") ApplicationUser user) {
 
         userRepo.save(user);
 
